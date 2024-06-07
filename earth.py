71a5eea2d01fd96c5f8f11ae8ebd7cdcc1b20a70
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-06-07 11:45:28 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
-def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+def compare_countries(country1, country2):
+    print("The result of " + str(country1) + " comes first in the dictionary than " + str(country2) + " is " + str(country1 < country2))
+    print("The result of " + str(country2) + " comes first in the dictionary than " + str(country1) + " is " + str(country2 < country1))
+
+X = "Bangladesh"
+Y = "Barbados"
+
+compare_countries(X, Y)
+
